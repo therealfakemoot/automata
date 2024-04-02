24f433793352a907b4c2602ae6ceba9bc380bf44
Nick Dumas <drakonik@gmail.com>
2024-04-02 14:59:38 -0400

    ECS sysstem coming together.
    The rules, the grid setup, and rendering are all abstracted out and
    swappable.

--- cmd/riser/main.go
+++ cmd/riser/main.go
@@ -1,30 +1,31 @@
 package main
 
 import (
-	"math/rand"
+	"flag"
 	"time"
 	// "fmt"
 
+	"github.com/EngoEngine/ecs"
 	"github.com/therealfakemoot/automata"
 )
 
 func main() {
+	var width, height int
+	var sleep time.Duration
+
+	flag.IntVar(&width, "width", 100, "horizontal width of automata grid")
+	flag.IntVar(&height, "height", 0, "height of automata grid; defaults to 0 for one-dimensional automata")
+	flag.DurationVar(&sleep, "cycle", 1*time.Second, "real-time duration of one evaluation cycle")
+
+	world := ecs.World{}
 	trs := automata.TerminalRenderSystem{
-		Width: 140,
+		Width: width,
 	}
-	trs.Grid = make([]automata.Cell, trs.Width)
+	trs.Grid = automata.NewGrid(width, height, automata.SeedConstant(1))
+	world.AddSystem(&trs)
 
-	for idx := range trs.Grid {
-		trs.Grid[idx].X = idx
-		if rand.Float64() > .70 {
-			trs.Grid[idx].State = 1
-		}
-	}
-
-	trs.Grid[3].State = 1
-
-	for {
-		trs.Update(1)
-		time.Sleep(500 * time.Millisecond)
+	t := time.Tick(sleep)
+	for range t {
+		world.Update(1)
 	}
 }
--- grid.go
+++ grid.go
@@ -1,5 +1,11 @@
 package automata
 
+import (
+	// "log"
+	"math"
+	"math/rand"
+)
+
 type Grid struct {
 	Width int
 	Cells []Cell
@@ -12,6 +18,47 @@ func (g *Grid) Update() {
 			g.Cells[cellIdx] = rule.Apply(cell)
 		}
 	}
+}
+
+type GridSeeder func([]Cell) []Cell
+
+func SeedIdentity(grid []Cell) []Cell {
+	return grid
+}
+
+func SeedConstant(c int) GridSeeder {
+	return func(grid []Cell) []Cell {
+		for idx := range grid {
+			grid[idx].State = c
+		}
+		return grid
+	}
+}
+
+func SeedRandom(x float64) GridSeeder {
+	return func(grid []Cell) []Cell {
+		for idx := range grid {
+			grid[idx].X = idx
+			if rand.Float64() >= x {
+				grid[idx].State = 1
+			}
+		}
+		return grid
+	}
+}
+
+func NewGrid(x, y int, seed GridSeeder) []Cell {
+	gridLen := int(math.Max(float64(x)*float64(y), float64(x)))
+	grid := make([]Cell, gridLen)
+	for i := 0; i < x; i++ {
+		for j := 0; j < y; j++ {
+			unwrappedIdx := (i * x) + j
+			grid[unwrappedIdx].X = i
+			grid[unwrappedIdx].Y = i
+		}
+	}
+
+	return seed(grid)
 }
 
 type Rule interface {
