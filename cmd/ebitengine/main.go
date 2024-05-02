271699a7512b998f6be504446b711aaa201e0243
Nick Dumas <drakonik@gmail.com>
2024-05-02 10:14:05 -0400

    Superficially have automata hooked into ebiten
    I'm pretty sure it's working, because it's drawing all black pixels,
    which is the problem i'm having with the rules: something is making the
    starting state converge to all-dead when it shouldn't.

--- cmd/ebitengine/main.go
+++ cmd/ebitengine/main.go
@@ -4,14 +4,38 @@ import (
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+
+	"github.com/therealfakemoot/automata"
 )
 
 type Game struct {
 	width, height int
 	pixels        []byte
+	world         *automata.Grid
+	rule          automata.Rule
 }
 
 func (g *Game) Update() error {
+	g.world.Update(g.rule)
+	for i := 0; i < g.height; i++ {
+		for j := 0; j < g.width; j++ {
+			cell := g.world.Get(i, j)
+			pixelIdx := i + (i * j)
+			switch cell {
+			case 0:
+				g.pixels[pixelIdx*4] = 0
+				g.pixels[pixelIdx*4+1] = 0
+				g.pixels[pixelIdx*4+2] = 0
+				g.pixels[pixelIdx*4+3] = 0
+			case 1:
+				g.pixels[pixelIdx*4] = 0xff
+				g.pixels[pixelIdx*4+1] = 0xff
+				g.pixels[pixelIdx*4+2] = 0xff
+				g.pixels[pixelIdx*4+3] = 0xff
+			}
+		}
+	}
+
 	return nil
 }
 
@@ -19,15 +43,24 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screen.WritePixels(g.pixels)
 }
 
-func (g *Game) Layout(outwideWidth, outsideHeight int) (int, int) {
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return g.width, g.height
 }
 
 func main() {
 	gameWidth, gameHeight := 300, 300
-	g := &Game{width: gameWidth, height: gameHeight, pixels: make([]byte, gameWidth*gameHeight*4)}
+	world := automata.NewGrid(gameWidth, gameHeight, automata.SeedCenter)
+	g := &Game{
+		width:  gameWidth,
+		height: gameHeight,
+		pixels: make([]byte,
+			gameWidth*gameHeight*4),
+		world: &world,
+		rule:  automata.Rule30,
+	}
 	ebiten.SetWindowSize(gameWidth*2, gameHeight*2)
 	ebiten.SetWindowTitle("Automata Demo")
+
 	if err := ebiten.RunGame(g); err != nil {
 		log.Fatalf("game error: %s\n", err)
 	}
