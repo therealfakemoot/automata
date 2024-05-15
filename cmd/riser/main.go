567f44606a4dd8a9f28a9cd0a044867db704be71
Nick Dumas <drakonik@gmail.com>
2024-05-15 09:30:25 -0400

    debug stuff for riser

--- cmd/riser/main.go
+++ cmd/riser/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
-	// "fmt"
+	"fmt"
+	"log"
 	"math"
+	"os"
 	"time"
 
 	"github.com/therealfakemoot/automata"
@@ -14,14 +16,21 @@ func main() {
 		width, height int
 		sleep         time.Duration
 		generations   int
+		outFile       string
 	)
 
 	flag.IntVar(&width, "width", 100, "horizontal width of automata grid")
 	flag.IntVar(&height, "height", 1, "height of automata grid; defaults to 1 for one-dimensional automata")
 	flag.IntVar(&generations, "generations", math.MaxInt, "number of automata iterations to run, -1 runs forever")
 	flag.DurationVar(&sleep, "cycle", 1*time.Second, "real-time duration of one evaluation cycle")
+	flag.StringVar(&outFile, "outFile", "./game.log", "Destination file for game updates.")
 
 	flag.Parse()
+
+	f, err := os.Create(outFile)
+	if err != nil {
+		log.Fatalf("couldn't open game file: %s", err)
+	}
 
 	g := automata.NewGrid(width, height, automata.SeedCenter)
 	defer g.Logger.Sync()
@@ -31,7 +40,7 @@ func main() {
 		if g.Generation >= generations {
 			return
 		}
-		// fmt.Printf("%s", g.String())
+		fmt.Fprintf(f, "%s", g.String())
 		g.Update(automata.Rule30)
 	}
 }
